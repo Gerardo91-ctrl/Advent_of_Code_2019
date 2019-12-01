74ad8d7a20d06591824ea5e09e29368080a570f6
GerardoLG <27868332+gerardolg@users.noreply.github.com>
2019-12-01 13:44:23 -0600

    First Half

---
+++ Day One/First Half/app.py
@@ -0,0 +1,9 @@
+with open('modules.txt', 'r') as file:
+    content = file.readlines()
+
+
+content = [int(x.strip()) for x in content]
+
+get_fuel_amount = sum([con//3-2 for con in content])
+
+print(get_fuel_amount)
