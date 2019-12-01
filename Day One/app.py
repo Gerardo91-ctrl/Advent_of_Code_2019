9832b3e49552e34de6609c7407897f7c95d1691a
GerardoLG <27868332+gerardolg@users.noreply.github.com>
2019-12-01 13:45:13 -0600

    Deleted duplicated files

--- Day One/app.py
+++
@@ -1,9 +0,0 @@
-with open('modules.txt', 'r') as file:
-    content = file.readlines()
-
-
-content = [int(x.strip()) for x in content]
-
-get_fuel_amount = sum([con//3-2 for con in content])
-
-print(get_fuel_amount)
